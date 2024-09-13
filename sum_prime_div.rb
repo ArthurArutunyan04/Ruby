3d958f19cf403081399fe07c8209e61d6aaa1a6b
Artur <larturl2004@yandex.ru>
2024-09-13 14:55:09 +0300

    Update sum_prime_div.rb

--- sum_prime_div.rb
+++ sum_prime_div.rb
@@ -1,4 +1,4 @@
-def sum_prime_divs(number)
+def sum_prime_divs(number)  #функция для нахождения суммы простых делителей
   sum = 0
   for i in 2..number
     if number % i == 0 && prime?(i)
@@ -8,8 +8,11 @@ def sum_prime_divs(number)
   sum
 end
 
-def prime?(div)
-  return false if div < 2
+def prime?(div)  #функция для проверки на простоту 
+  if div < 2
+    return false
+  end
+  
   for i in 2..(div - 1)
     if div % i == 0
       return false
@@ -19,6 +22,6 @@ def prime?(div)
 end
 
 print "Введите число: "
-number = gets.chomp.to_i
+number = gets.chomp.to_i #Считывает введенное число, удаляет символ новой строки и преобразовывает ццисло в целое 
 function_1 = sum_prime_divs(number)
 print("Сумма простых делителей: #{function_1}\n")
