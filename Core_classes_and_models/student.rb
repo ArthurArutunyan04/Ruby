e5b65e933242ec4d9b969b34fa8f955135b9128e
Artur <larturl2004@yandex.ru>
2024-09-23 10:07:41 +0300

    Lb2.Task1.4 named args

--- Core_classes_and_models/student.rb
+++ Core_classes_and_models/student.rb
@@ -2,7 +2,7 @@ class Student
   
   attr_accessor :id, :surname, :name, :patronymic, :phone, :telegram, :email, :git
 
-  def initialize(surname, name, patronymic,id = nil,phone = nil, telegram = nil, email = nil, git = nil)
+  def initialize(surname, name, patronymic, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
     @id = id
     @surname = surname
     @name = name
