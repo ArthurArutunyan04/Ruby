67994c078f907f633c30bce93913e8010cdf2033
Artur <larturl2004@yandex.ru>
2024-09-23 10:42:13 +0300

    Lb2.Task1.8 validate other fields

--- Core_classes_and_models/student.rb
+++ Core_classes_and_models/student.rb
@@ -1,46 +1,61 @@
-class Student
-  attr_accessor :id, :surname, :name, :patronymic, :telegram, :email, :git
-  attr_reader :phone
+  class Student
 
-  def initialize(surname:, name:, patronymic:, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
-    self.id = id
-    self.surname = surname
-    self.name = name
-    self.patronymic = patronymic
-    self.phone = phone
-    self.telegram = telegram
-    self.email = email
-    self.git = git
-  end
+    attr_reader :id, :surname, :name, :patronymic, :telegram, :email, :git, :phone
 
-  def to_s
-    "
-    ID = #{@id || "Пусто"}
-    Surname = #{@surname}
-    Name = #{@name}
-    Patronymic = #{@patronymic}
-    Phone = #{@phone || "Пусто"}
-    Telegram = #{@telegram || "Пусто"}
-    Email = #{@email || "Пусто"}
-    Git = #{@git || "Пусто"}
-    "
-  end
-
-  def self.valid_phone?(phone)
-    phone =~ /^\+\d{11}$/  
-  end
-
-  def phone=(phone)
-    if Student.valid_phone?(phone)
-      @phone = phone
-    else
-      @phone = nil  
+    def initialize(surname:, name:, patronymic:, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
+      self.id = id
+      self.surname = surname
+      self.name = name
+      self.patronymic = patronymic
+      self.phone = phone
+      self.telegram = telegram
+      self.email = email
+      self.git = git
     end
-  end
-end
-  
-student1 = Student.new(name: "Pudge", surname: "MoM", patronymic: "Midas", phone: "+77777777777")
-student2 = Student.new(name: "Петр", surname: "Петров", patronymic: "Петрович", phone: "123zxcgg")
 
-puts student1.to_s
-puts student2.to_s
+    def to_s
+      "
+      ID = #{@id || "Пусто"}
+      Surname = #{@surname}
+      Name = #{@name}
+      Patronymic = #{@patronymic}
+      Phone = #{@phone || "Пусто"}
+      Telegram = #{@telegram || "Пусто"}
+      Email = #{@email || "Пусто"}
+      Git = #{@git || "Пусто"}
+      "
+    end
+    
+    def phone=(phone)
+        @phone = phone =~ /^\+\d{11}$/ ? phone : nil
+      end
+    
+      def id=(id)
+        @id = id.to_s =~ /^\d+$/ ? id : nil
+      end
+    
+      def surname=(surname)
+        @surname = surname =~ /^[A-ZА-Я][a-zа-я]*$/ ? surname : nil
+      end
+    
+      def name=(name)
+        @name = name =~ /^[A-ZА-Я][a-zа-я]*$/ ? name : nil
+      end
+    
+      def patronymic=(patronymic)
+        @patronymic = patronymic =~ /^[A-ZА-Я][a-zа-я]*$/ ? patronymic : nil
+      end
+    
+      def email=(email)
+        @email = email =~ /^[a-zA-Z0-9._%+-]{3,10}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/ ? email : nil
+      end
+    
+      def telegram=(telegram)
+        @telegram = telegram =~ /^@[a-zA-Z0-9_]{1,}$/ ? telegram : nil
+      end
+    
+      def git=(git)
+        @git = git =~ /^https:\/\/github\.com\/[a-zA-Z0-9_-]+\/?$/ ? git : nil
+      end
+  end
+    
