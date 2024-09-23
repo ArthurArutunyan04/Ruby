b46f25f07d098079d8d43146f9814c6e7a053b3d
Artur <larturl2004@yandex.ru>
2024-09-23 09:56:33 +0300

    Lb2.Task1.2 added to_s

--- Core_classes_and_models/student.rb
+++ Core_classes_and_models/student.rb
@@ -1,77 +1,89 @@
 class Student
   def initialize(surname, name, patronymic,id = nil,phone = nil, telegram = nil, email = nil, git = nil)
-    @id = id
-    @surname = surname
-    @name = name
-    @patronymic = patronymic
-    @phone = phone
-    @telegram = telegram
-    @email = email
-    @git = git
+      @id = id
+      @surname = surname
+      @name = name
+      @patronymic = patronymic
+      @phone = phone
+      @telegram = telegram
+      @email = email
+      @git = git
   end
-
   
   def id
-    @id
+      @id
   end
-
+  
   def surname
-    @surname
+      @surname
   end
-
+  
   def name
-    @name
+      @name
   end
-
+  
   def patronymic
-    @patronymic
+      @patronymic
   end
-
+  
   def phone
-    @phone
+      @phone
   end
-
+  
   def telegram
-    @telegram
+      @telegram
   end
-
+  
   def email
-    @email
+      @email
   end
-
+  
   def git
-    @git
+      @git
   end
-
+  
   def id=(value)
-    @id = value
+      @id = value
   end
-
+  
   def surname=(value)
-    @last_name = value
+      @last_name = value
   end
-
-  def name= (value)
-    @name = value
+  
+  def name=(value)
+      @name = value
   end
-
+  
   def patronymic=(value)
-    @patronymic = value
+      @patronymic = value
   end
-
+  
   def phone=(value)
-    @phone = value
+      @phone = value
   end
-
+  
   def telegram=(value)
-    @telegram = value
+      @telegram = value
   end
-
+  
   def email=(value)
-    @email = value
+      @email = value
   end
-
+  
   def git=(value)
-    @git = value
+      @git = value
+  end
+  
+  def to_s()
+      "
+      ID = #{@id || "Пусто"}
+      Surname = #{@surname}
+      Name = #{@name}
+      Patronymic = #{@patronymic}
+      Phone = #{@phone || "Пусто"}
+      Telegram = #{@telegram || "Пусто"}
+      Email = #{@email || "Пусто"}
+      Git = #{@git || "Пусто"}
+      "
   end
 end
