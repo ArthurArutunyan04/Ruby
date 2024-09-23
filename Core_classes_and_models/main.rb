6c90f7a14afc701542ace98721774111828dc663
Artur <larturl2004@yandex.ru>
2024-09-23 09:54:11 +0300

    Lb2.Task1.2 class main

---
+++ Core_classes_and_models/main.rb
@@ -0,0 +1,7 @@
+require_relative   'class.rb'
+
+student1 = Student.new("Сервер", "Линукс", "Бэкендович", 1, "+77777777777", nil, "Sigma228@rambler.ru", nil)
+student2 = Student.new("Адидас", "Найк", "НьюБелансавич", 12, "+79234446661","NikePro", nil, "tgk")
+
+puts student1
+puts student2
