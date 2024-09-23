f1de4cfde15e0a560099f72bb899602d6a39e111
Artur <larturl2004@yandex.ru>
2024-09-23 10:34:47 +0300

    Lb2.Task1.7 check phone number

--- Core_classes_and_models/student.rb
+++ Core_classes_and_models/student.rb
@@ -1,6 +1,6 @@
 class Student
-  
-  attr_accessor :id, :surname, :name, :patronymic, :phone, :telegram, :email, :git
+  attr_accessor :id, :surname, :name, :patronymic, :telegram, :email, :git
+  attr_reader :phone
 
   def initialize(surname:, name:, patronymic:, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
     self.id = id
@@ -13,8 +13,7 @@ class Student
     self.git = git
   end
 
-
-  def to_s()
+  def to_s
     "
     ID = #{@id || "Пусто"}
     Surname = #{@surname}
@@ -26,4 +25,22 @@ class Student
     Git = #{@git || "Пусто"}
     "
   end
+
+  def self.valid_phone?(phone)
+    phone =~ /^\+\d{11}$/  
+  end
+
+  def phone=(phone)
+    if Student.valid_phone?(phone)
+      @phone = phone
+    else
+      @phone = nil  
+    end
+  end
 end
+  
+student1 = Student.new(name: "Pudge", surname: "MoM", patronymic: "Midas", phone: "+77777777777")
+student2 = Student.new(name: "Петр", surname: "Петров", patronymic: "Петрович", phone: "123zxcgg")
+
+puts student1.to_s
+puts student2.to_s
