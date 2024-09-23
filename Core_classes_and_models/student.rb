adbee241683bb9c8efb7e23c048d3da468dc950a
Artur <larturl2004@yandex.ru>
2024-09-23 10:00:31 +0300

    Lb2.Task1.3 avoided code duplication

--- Core_classes_and_models/student.rb
+++ Core_classes_and_models/student.rb
@@ -1,89 +1,29 @@
 class Student
+  
+  attr_reader :id, :surname, :name, :patronymic, :phone, :telegram, :email, :git
+  attr_writer :id, :surname, :name, :patronymic, :phone, :telegram, :email, :git
+
   def initialize(surname, name, patronymic,id = nil,phone = nil, telegram = nil, email = nil, git = nil)
-      @id = id
-      @surname = surname
-      @name = name
-      @patronymic = patronymic
-      @phone = phone
-      @telegram = telegram
-      @email = email
-      @git = git
+    @id = id
+    @surname = surname
+    @name = name
+    @patronymic = patronymic
+    @phone = phone
+    @telegram = telegram
+    @email = email
+    @git = git
   end
-  
-  def id
-      @id
-  end
-  
-  def surname
-      @surname
-  end
-  
-  def name
-      @name
-  end
-  
-  def patronymic
-      @patronymic
-  end
-  
-  def phone
-      @phone
-  end
-  
-  def telegram
-      @telegram
-  end
-  
-  def email
-      @email
-  end
-  
-  def git
-      @git
-  end
-  
-  def id=(value)
-      @id = value
-  end
-  
-  def surname=(value)
-      @last_name = value
-  end
-  
-  def name=(value)
-      @name = value
-  end
-  
-  def patronymic=(value)
-      @patronymic = value
-  end
-  
-  def phone=(value)
-      @phone = value
-  end
-  
-  def telegram=(value)
-      @telegram = value
-  end
-  
-  def email=(value)
-      @email = value
-  end
-  
-  def git=(value)
-      @git = value
-  end
-  
+
   def to_s()
-      "
-      ID = #{@id || "Пусто"}
-      Surname = #{@surname}
-      Name = #{@name}
-      Patronymic = #{@patronymic}
-      Phone = #{@phone || "Пусто"}
-      Telegram = #{@telegram || "Пусто"}
-      Email = #{@email || "Пусто"}
-      Git = #{@git || "Пусто"}
-      "
+    "
+    ID = #{@id || "Пусто"}
+    Surname = #{@surname}
+    Name = #{@name}
+    Patronymic = #{@patronymic}
+    Phone = #{@phone || "Пусто"}
+    Telegram = #{@telegram || "Пусто"}
+    Email = #{@email || "Пусто"}
+    Git = #{@git || "Пусто"}
+    "
   end
 end
