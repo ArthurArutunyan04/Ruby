c692fd5dd8e56f0a95210d001d242a67155e6435
Artur <larturl2004@yandex.ru>
2024-09-27 13:16:18 +0300

    Lb2.Task1.10 set_contacts

--- Core_classes_and_models/student.rb
+++ Core_classes_and_models/student.rb
@@ -1,73 +1,78 @@
-  class Student
+class Student
+  attr_reader :id, :surname, :name, :patronymic, :telegram, :email, :git, :phone
 
-    attr_reader :id, :surname, :name, :patronymic, :telegram, :email, :git, :phone
-
-    def initialize(surname:, name:, patronymic:, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
-      self.id = id
-      self.surname = surname
-      self.name = name
-      self.patronymic = patronymic
-      self.phone = phone
-      self.telegram = telegram
-      self.email = email
-      self.git = git
-    end
-
-    def to_s
-      "
-      ID = #{@id || "Пусто"}
-      Surname = #{@surname}
-      Name = #{@name}
-      Patronymic = #{@patronymic}
-      Phone = #{@phone || "Пусто"}
-      Telegram = #{@telegram || "Пусто"}
-      Email = #{@email || "Пусто"}
-      Git = #{@git || "Пусто"}
-      "
-    end
-    
-    def phone=(phone)
-        @phone = phone =~ /^\+\d{11}$/ ? phone : nil
-      end
-    
-      def id=(id)
-        @id = id.to_s =~ /^\d+$/ ? id : nil
-      end
-    
-      def surname=(surname)
-        @surname = surname =~ /^[A-ZА-Я][a-zа-я]*$/ ? surname : nil
-      end
-    
-      def name=(name)
-        @name = name =~ /^[A-ZА-Я][a-zа-я]*$/ ? name : nil
-      end
-    
-      def patronymic=(patronymic)
-        @patronymic = patronymic =~ /^[A-ZА-Я][a-zа-я]*$/ ? patronymic : nil
-      end
-    
-      def email=(email)
-        @email = email =~ /^[a-zA-Z0-9._%+-]{3,10}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/ ? email : nil
-      end
-    
-      def telegram=(telegram)
-        @telegram = telegram =~ /^@[a-zA-Z0-9_]{1,}$/ ? telegram : nil
-      end
-    
-      def git=(git)
-        @git = git =~ /^https:\/\/github\.com\/[a-zA-Z0-9_-]+\/?$/ ? git : nil
-      end
-
-       def validate
-        validate_git && validate_contact
-      end
-    
-      def validate_git
-        !@git.nil?
-      end
-    
-      def validate_contact
-        !@phone.nil? || !@telegram.nil? || !@email.nil?
-      end
+  def initialize(surname:, name:, patronymic:, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
+    self.id = id
+    self.surname = surname
+    self.name = name
+    self.patronymic = patronymic
+    set_contacts(phone: phone, telegram: telegram, email: email, git: git)
   end
-    
+
+  def to_s
+    "
+    ID = #{@id || "Пусто"}
+    Surname = #{@surname}
+    Name = #{@name}
+    Patronymic = #{@patronymic}
+    Phone = #{@phone || "Пусто"}
+    Telegram = #{@telegram || "Пусто"}
+    Email = #{@email || "Пусто"}
+    Git = #{@git || "Пусто"}
+    "
+  end
+
+  def set_contacts(phone: nil, telegram: nil, email: nil, git: nil)
+    self.phone = phone
+    self.telegram = telegram
+    self.email = email
+    self.git = git
+  end
+
+  def id=(id)
+    @id = id.to_s =~ /^\d+$/ ? id : nil
+  end
+
+  def surname=(surname)
+    @surname = surname =~ /^[A-ZА-Я][a-zа-я]+$/ ? surname : nil
+  end
+
+  def name=(name)
+    @name = name =~ /^[A-ZА-Я][a-zа-я]+$/ ? name : nil
+  end
+
+  def patronymic=(patronymic)
+    @patronymic = patronymic =~ /^[A-ZА-Я][a-zа-я]*$/ ? patronymic : nil
+  end
+
+  private
+
+  def phone=(phone)
+    @phone = phone =~ /^\+\d{11}$/ ? phone : nil
+  end
+
+  def telegram=(telegram)
+    @telegram = telegram =~ /^@[a-zA-Z0-9_]{1,}$/ ? telegram : nil
+  end
+
+  def email=(email)
+    @email = email =~ /^[a-zA-Z0-9.%+-]{3,10}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/ ? email : nil
+  end
+
+  def git=(git)
+    @git = git =~ /^https:\/\/github.com\/[a-zA-Z0-9-]+\/?$/ ? git : nil
+  end
+
+  def validate
+    validate_git && validate_contact
+  end
+
+  def validate_git
+    !@git.nil?
+  end
+
+  def validate_contact
+    !@phone.nil? && !@telegram.nil? && !@email.nil?
+  end
+end
+
