9314e1cbc53f332578f316dc223c567e5b5cf00a
Artur <larturl2004@yandex.ru>
2024-10-11 16:36:57 +0300

    Lb2.Task2.3,2.5 refactoring and removed other classes

--- Core_classes_and_models/student.rb
+++ Core_classes_and_models/student.rb
@@ -1,58 +1,7 @@
-class Human
-  
-  attr_reader :surname_with_initials, :git, :contact
-
-  def initialize(surname:, name:, patronymic:, git:, phone: nil, telegram: nil, email: nil)
-    @surname_with_initials = "#{surname} #{name[0]}.#{patronymic[0]}."
-    self.git = git
-    @contact = determine_contact(phone, telegram, email)
-  end
-
-  def determine_contact(phone, telegram, email)
-    contact_info = phone || telegram || email || "Не указано"
-    case contact_info
-    when phone
-      "Phone: #{phone}"
-    when telegram
-      "Telegram: #{telegram}"
-    when email
-      "Email: #{email}"
-    else
-      "Не указано"
-    end
-  end
-
-  def to_s
-    output = []
-    output << "#{@surname_with_initials}"
-    output << "Git: #{@git}" 
-    output << "Контакт: #{@contact}"
-    output.join("; ")
-  end
-
-  private
-
-  def phone=(phone)
-    @phone = phone =~ /^\+\d{11}$/ ? phone : nil
-  end
-
-  def telegram=(telegram)
-    @telegram = telegram =~ /^@[a-zA-Z0-9_]{1,}$/ ? telegram : nil
-  end
-
-  def email=(email)
-    @email = email =~ /^[a-zA-Z0-9.%+-]{3,10}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/ ? email : nil
-  end
-
-  def git=(git)
-    @git = git =~ /^https:\/\/github.com\/[a-zA-Z0-9-]+\/?$/ ? git : nil
-  end
-end
-
+require_relative 'human'
 
 class Student < Human
-  
-  attr_reader :id, :phone, :telegram, :email, :patronymic
+  attr_reader :id, :patronymic
 
   def initialize(surname:, name:, patronymic:, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
     super(surname: surname, name: name, patronymic: patronymic, git: git, phone: phone, telegram: telegram, email: email)
@@ -73,16 +22,8 @@ class Student < Human
     "
   end
 
-
   def getInfo
     "#{surname_with_initials}; Git: #{@git}; Контакт: #{contact}"
-  end
-
-  def set_contacts(phone: nil, telegram: nil, email: nil, git: nil)
-    self.phone = phone
-    self.telegram = telegram
-    self.email = email
-    self.git = git
   end
 
   def id=(id)
@@ -101,45 +42,4 @@ class Student < Human
     @patronymic = patronymic =~ /^[A-ZА-Я][a-zа-я]*$/ ? patronymic : nil
   end
 end
-
-
-class Student_short < Human
-  
-  attr_reader :id
-
-  def initialize(student: nil, id: nil, info_string: nil)
-    if student
-      super(
-        surname: student.surname_with_initials.split[0],
-        name: student.surname_with_initials.split[1][0],
-        patronymic: student.surname_with_initials.split[1][2],
-        git: student.git,
-        phone: student.phone,
-        telegram: student.telegram,
-        email: student.email
-      )
-    elsif id && info_string
-      @id = id
-      parse_info_string(info_string)
-    end
-  end
-
-  def to_s
-    output = []
-    output << "ID: #{@id}" if @id
-    output << super
-    output.join("; ")
-  end
-
-  private
-
-  def parse_info_string(info_string)
-    details = info_string.split(';')
-    surname, name, patronymic, phone, telegram, email, git = details.map(&:strip)
-    @surname_with_initials = "#{surname} #{name[0]}.#{patronymic[0]}."
-    self.git = git
-    @contact = determine_contact(phone, telegram, email)
-  end
-end
-
 
