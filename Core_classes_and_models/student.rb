980b4c9263916207453d6932f7d908cf3436c8cb
Artur <larturl2004@yandex.ru>
2024-10-18 23:42:41 +0300

    Lb2.Task2.5 logical update

--- Core_classes_and_models/student.rb
+++ Core_classes_and_models/student.rb
@@ -1,21 +1,22 @@
 require_relative 'human'
 
 class Student < Human
-  attr_reader :id, :surname, :name, :patronymic
+  attr_reader :id, :surname, :name, :patronymic, :git
 
   def initialize(surname:, name:, patronymic:, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
-    super(surname: surname, name: name, patronymic: patronymic, git: git, phone: phone, telegram: telegram, email: email)
+    super(surname: surname, name: name, patronymic: patronymic, phone: phone, telegram: telegram, email: email)
     self.id = id
     self.surname = surname
     self.name = name
     self.patronymic = patronymic
+    self.git = git
   end
 
   def to_s
     "
     ID = #{@id || 'Пусто'}
     SWI = #{@surname_with_initials || 'Пусто'}
-    Phone = #{@phone || 'Пусто'}
+    Phone = #{@phone}
     Telegram = #{@telegram || 'Пусто'}
     Email = #{@email || 'Пусто'}
     Git = #{@git || 'Пусто'}
@@ -24,12 +25,6 @@ class Student < Human
 
   def getInfo
     "#{surname_with_initials}; Git: #{@git}; Контакт: #{contact}"
-  end
-
-  private
-  
-  def id=(id)
-    @id = id.to_s =~ /^\d+$/ ? id : nil
   end
 
   def surname=(surname)
@@ -42,6 +37,16 @@ class Student < Human
 
   def patronymic=(patronymic)
     @patronymic = patronymic =~ /^[A-ZА-Я][a-zа-я]*$/ ? patronymic : nil
+  end
+
+  def git=(git)
+    @git = git =~ /^https:\/\/github.com\/[a-zA-Z0-9-]+\/?$/ ? git : nil
+  end
+
+  private
+  
+  def id=(id)
+    @id = id.to_s =~ /^\d+$/ ? id : nil
   end
 end
 
