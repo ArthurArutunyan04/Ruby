e68ed74ccc0490bd5746a1e33bc47f11049b787d
Artur <larturl2004@yandex.ru>
2024-11-01 14:29:57 +0300

    Lb3.Task2.3 add method none?

--- blocks_collections_trees/array_processor.rb
+++ blocks_collections_trees/array_processor.rb
@@ -26,8 +26,22 @@ class Array_processor
     nil
   end
 
+  def none?
+    if !block_given?
+      return self.array.empty?
+    else
+      self.array.each do |element|
+        if yield(element)
+          return false
+        end
+      end
+    end
+    true
+  end
+
+
   def to_s
-    array.join(', ')
+    self.array.join(', ')
   end
 end
 
@@ -35,3 +49,5 @@ processor = Array_processor.new([1, 12, 63, 24 ,95, 4, 77])
 puts processor
 puts processor.any? {|element| element < 0}
 puts processor.find_index {|element| element == 63}
+puts processor.none?
+puts processor.none?{|element| element > 1111}
