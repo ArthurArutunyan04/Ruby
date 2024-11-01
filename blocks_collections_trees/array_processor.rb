29a4225f73b799bec61bce3ca970e94953de2dd7
Artur <larturl2004@yandex.ru>
2024-11-01 21:16:19 +0300

    Lb3.Task2.6 add method find_all

--- blocks_collections_trees/array_processor.rb
+++ blocks_collections_trees/array_processor.rb
@@ -67,6 +67,17 @@ class Array_processor
     [min, max]
   end
 
+  def find_all
+    result = []
+
+    self.array.each do |element|
+      if yield(element)
+        result << element
+      end
+    end
+    result
+  end
+
   def to_s
     self.array.join(', ')
   end
@@ -76,9 +87,10 @@ processor = Array_processor.new([1, 12, 63, 24 ,95, 4, 77])
 puts processor
 puts processor.any? {|element| element < 0}
 puts processor.any?
-# puts processor.find_index {|element| element == 63}
+puts processor.find_index {|element| element == 63}
 puts processor.none?
 puts processor.none?{|element| element > 1111}
 puts processor.reduce{|accumulator, element| accumulator + element}
 puts processor.reduce(12){|accumulator, element| accumulator + element}
 puts processor.min_max{|element1, element2| element1 <=> element2}
+puts processor.find_all{|element| element.odd?}
