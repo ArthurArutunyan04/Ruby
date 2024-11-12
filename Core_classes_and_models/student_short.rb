1ceb06e9ddec9e34508bab1b497e2890989064ce
Artur <larturl2004@yandex.ru>
2024-11-12 18:18:34 +0300

    Lb2.Task2.5 improved method from_info_string

--- Core_classes_and_models/student_short.rb
+++ Core_classes_and_models/student_short.rb
@@ -2,7 +2,6 @@ require_relative 'human'
 
 class Student_short < Human
   attr_accessor :id
-
   def self.from_student(student)
     student_short = new(
       surname: student.surname_with_initials.split[0],
@@ -11,14 +10,15 @@ class Student_short < Human
       git: student.git,
       phone: student.phone,
       telegram: student.telegram,
-      email: student.email
+      email: student.email,
+      id: student.id
     )
     student_short
   end
-
+  
   def self.from_info_string(id, info_string)
     surname, name, patronymic, phone, telegram, email, git = parse_info_string(info_string)
-
+    
     student_short = new(
       surname: surname,
       name: name,
@@ -26,16 +26,17 @@ class Student_short < Human
       git: git,
       phone: phone,
       telegram: telegram,
-      email: email
+      email: email,
+      id: id
     )
-    student_short.id = id
     student_short
   end
 
   def self.parse_info_string(info_string)
     details = info_string.split(';')
-    details.map(&:strip) 
+    details.map(&:strip)
   end
+
 
   def to_s
     output = []
@@ -44,9 +45,10 @@ class Student_short < Human
     output.join("; ")
   end
 
+
   private_class_method :new
 
-  def initialize(surname:, name:, patronymic:, git:, phone: nil, telegram: nil, email: nil)
-    super(surname: surname, name: name, patronymic: patronymic, git: git, phone: phone, telegram: telegram, email: email)
+  def initialize(id: nil, surname:, name:, patronymic:, git: nil, phone: nil, telegram: nil, email: nil)
+    super(id: id, surname: surname, name: name, patronymic: patronymic, git: git, phone: phone, telegram: telegram, email: email)
   end
 end
