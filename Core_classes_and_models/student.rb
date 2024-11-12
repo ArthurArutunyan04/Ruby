e8f5aea735cb293db1cb97a320a1f79aee763bba
Artur <larturl2004@yandex.ru>
2024-11-12 18:14:30 +0300

    Lb2.Task2.5 Refactoring

--- Core_classes_and_models/student.rb
+++ Core_classes_and_models/student.rb
@@ -1,52 +1,59 @@
 require_relative 'human'
 
 class Student < Human
-  attr_reader :id, :surname, :name, :patronymic, :git
+  attr_reader :surname, :name, :patronymic
 
   def initialize(surname:, name:, patronymic:, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
-    super(surname: surname, name: name, patronymic: patronymic, phone: phone, telegram: telegram, email: email)
-    self.id = id
+    super(id: id, surname: surname, name: name, patronymic: patronymic, phone: phone, telegram: telegram, email: email, git: git)
     self.surname = surname
     self.name = name
     self.patronymic = patronymic
-    self.git = git
+  end
+
+  def self.valid_surname?(surname)
+    surname.match?(/^[A-ZА-Я][a-zа-я]+$/)
+  end
+
+  def self.valid_name?(name)
+    name.match?(/^[A-ZА-Я][a-zа-я]+$/)
+  end
+
+  def self.valid_patronymic?(patronymic)
+    patronymic.match?(/^[A-ZА-Я][a-zа-я]+$/)
+  end
+
+  def surname=(surname)
+    if Student.valid_surname?(surname)
+      @surname = surname
+    else
+      raise ArgumentError.new("Некорректная фамилия")
+    end
+  end
+
+  def name=(name)
+    if Student.valid_name?(name)
+      @name = name
+    else
+      raise ArgumentError.new("Некорректное имя")
+    end
+  end
+
+  def patronymic=(patronymic)
+    if Student.valid_patronymic?(patronymic)
+      @patronymic = patronymic
+    else
+      raise ArgumentError.new("Некорректное отчество")
+    end
   end
 
   def to_s
     "
     ID = #{@id || 'Пусто'}
-    SWI = #{@surname_with_initials || 'Пусто'}
-    Phone = #{@phone}
+    SWI = #{generate_surname_with_initials(@surname, @name, @patronymic)}
+    Phone = #{@phone || 'Пусто'}
     Telegram = #{@telegram || 'Пусто'}
     Email = #{@email || 'Пусто'}
     Git = #{@git || 'Пусто'}
     "
   end
-
-  def getInfo
-    "#{surname_with_initials}; Git: #{@git}; Контакт: #{contact}"
-  end
-
-  def surname=(surname)
-    @surname = surname =~ /^[A-ZА-Я][a-zа-я]+$/ ? surname : nil
-  end
-
-  def name=(name)
-    @name = name =~ /^[A-ZА-Я][a-zа-я]+$/ ? name : nil
-  end
-
-  def patronymic=(patronymic)
-    @patronymic = patronymic =~ /^[A-ZА-Я][a-zа-я]*$/ ? patronymic : nil
-  end
-
-  def git=(git)
-    @git = git =~ /^https:\/\/github.com\/[a-zA-Z0-9-]+\/?$/ ? git : nil
-  end
-
-  private
-  
-  def id=(id)
-    @id = id.to_s =~ /^\d+$/ ? id : nil
-  end
 end
-
