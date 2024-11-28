ed2564eede0e1031a1487fbed1935f15122b74e9
ArthurArutunyan04 <larturl2004@yandex.ru>
2024-11-28 15:58:09 +0300

    Lb4.Task1.5 add class date_list_student_short

---
+++ Student/date/data_list_student_short.rb
@@ -0,0 +1,24 @@
+require_relative '../entities/student_short'
+
+class Data_list_student_short < Data_list
+  def get_names
+    ['surname_with_initials', 'contact', 'git']
+  end
+
+  def get_data
+    data = []
+
+    self.elements.each_with_index do |student_short, index|
+      row = [index + 1]
+
+      row << student_short.surname_with_initials
+      row << student_short.contact
+      row << student_short.git
+
+      data << row
+    end
+    Data_table.new(data)
+  end
+
+
+end
